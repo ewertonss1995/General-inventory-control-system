3e79d379086c2d5ee7826b756dc99ea638d0e7a9
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-07-14 09:43:35 -0300

    Projeto inventory-control-system criado

---
+++ inventory-control-system/src/main/java/com/inventory/control/system/InventoryControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.control.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryControlSystemApplication.class, args);
+	}
+
+}
---
+++ inventory-control-system/src/test/java/com/inventory/control/system/InventoryControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.control.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
