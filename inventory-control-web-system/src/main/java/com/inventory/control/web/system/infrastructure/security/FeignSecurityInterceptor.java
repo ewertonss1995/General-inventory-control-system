cd911a8a0dff8dd8a075d4bf0ff96d3d0a6a522d
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-07-19 07:52:25 -0300

    Criado FeignSecurityInterceptor

---
+++ inventory-control-web-system/src/main/java/com/inventory/control/web/system/infrastructure/security/FeignSecurityInterceptor.java
@@ -0,0 +1,25 @@
+package com.inventory.control.web.system.infrastructure.integration.security;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignSecurityInterceptor implements RequestInterceptor {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_TOKEN_TYPE = "Bearer";
+
+    @Override
+    public void apply(RequestTemplate template) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getCredentials() instanceof Jwt jwt) {
+            String tokenValue = jwt.getTokenValue();
+            template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, tokenValue));
+        }
+    }
+}
