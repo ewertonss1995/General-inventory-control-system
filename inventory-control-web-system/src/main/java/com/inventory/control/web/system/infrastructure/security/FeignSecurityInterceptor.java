b15a3e4f5286f7ed56d77a820d8c0bce8c047b65
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-07-19 07:57:53 -0300

    Atualizando a Configuração para Tornar a Propagação Opcional

--- inventory-control-web-system/src/main/java/com/inventory/control/web/system/infrastructure/security/FeignSecurityInterceptor.java
+++ inventory-control-web-system/src/main/java/com/inventory/control/web/system/infrastructure/security/FeignSecurityInterceptor.java
@@ -5,9 +5,7 @@ import feign.RequestTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.stereotype.Component;
 
-@Component
 public class FeignSecurityInterceptor implements RequestInterceptor {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
