f0e1a80457d603c0545cadb1d5987d71c7006582
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-07-20 12:25:37 -0300

    criação de repositories

---
+++ inventory-control-system/src/main/java/com/inventory/control/system/domain/repository/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.inventory.control.system.domain.repository;
+
+import com.inventory.control.system.domain.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
+    Optional<Category> findByNameIgnoreCase(String name);
+    
+    boolean existsByNameIgnoreCase(String name);
+}
