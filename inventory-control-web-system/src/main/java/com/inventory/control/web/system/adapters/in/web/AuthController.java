f0ed8939909237e6bbd7d341a633abd73be74d5b
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-07-29 06:23:27 -0300

    Ajustes na arquitetura para o fluxo de autenticação

--- inventory-control-web-system/src/main/java/com/inventory/control/web/system/adapters/in/web/AuthController.java
+++ inventory-control-web-system/src/main/java/com/inventory/control/web/system/adapters/in/web/AuthController.java
@@ -1,21 +1,32 @@
 package com.inventory.control.web.system.adapters.in.web;
 
 import com.inventory.control.web.system.adapters.in.web.dto.request.LoginRequest;
+import com.inventory.control.web.system.adapters.in.web.dto.request.RegisterUserRequest;
 import com.inventory.control.web.system.adapters.in.web.dto.response.TokenResponse;
 import com.inventory.control.web.system.ports.in.AuthenticateUserUseCase;
+import com.inventory.control.web.system.ports.in.RegisterUserUseCase;
+import com.inventory.control.web.system.ports.in.RegisterUserUseCase;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/authenticate")
-public class AuthenticateController {
+@RequestMapping("/api/auth")
+public class AuthController {
 
     private final AuthenticateUserUseCase authenticateUserUseCase;
+    private final RegisterUserUseCase registerUserUseCase;
 
-    public AuthenticateController(AuthenticateUserUseCase authenticateUserUseCase) {
+    public AuthController(AuthenticateUserUseCase authenticateUserUseCase, RegisterUserUseCase registerUserUseCase) {
         this.authenticateUserUseCase = authenticateUserUseCase;
+        this.registerUserUseCase = registerUserUseCase;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerUser(@Valid @RequestBody RegisterUserRequest request) {
+        registerUserUseCase.execute(request);
     }
 
     @PostMapping("/login")
--- inventory-control-web-system/src/main/java/com/inventory/control/web/system/adapters/out/auth/AuthUserAdapter.java
+++ inventory-control-web-system/src/main/java/com/inventory/control/web/system/adapters/out/auth/AuthUserAdapter.java
@@ -1,19 +1,26 @@
-package com.inventory.control.web.system.adapters.out.auth.authenticate;
+package com.inventory.control.web.system.adapters.out.auth;
 
 import com.inventory.control.web.system.adapters.out.auth.AuthFeignClient;
+import com.inventory.control.web.system.adapters.in.web.dto.request.RegisterUserRequest;
 import com.inventory.control.web.system.adapters.in.web.dto.request.LoginRequest;
 import com.inventory.control.web.system.adapters.in.web.dto.response.TokenResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.http.ResponseEntity;
 import com.inventory.control.web.system.ports.out.AuthenticateUserPort;
+import com.inventory.control.web.system.ports.out.CreateUserPort;
 
 @Component
-public class AuthenticateUserAdapter implements AuthenticateUserPort {
+public class AuthUserAdapter implements AuthenticateUserPort, CreateUserPort {
 
     private final AuthFeignClient authFeignClient;
 
-    public AuthenticateUserAdapter(AuthFeignClient authFeignClient) {
+    public AuthUserAdapter(AuthFeignClient authFeignClient) {
         this.authFeignClient = authFeignClient;
+    }
+
+    @Override
+    public void createUser(RegisterUserRequest registerUserRequest) {
+        authFeignClient.register(registerUserRequest);
     }
 
     @Override
