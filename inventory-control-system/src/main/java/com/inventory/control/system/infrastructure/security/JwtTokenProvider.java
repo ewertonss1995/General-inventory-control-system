adf020698fb70f3f3f53a79aac14d23ccdd0f068
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-08-11 13:08:40 -0300

    Ajuste JwtTokenProvider

--- inventory-control-system/src/main/java/com/inventory/control/system/infrastructure/security/JwtTokenProvider.java
+++ inventory-control-system/src/main/java/com/inventory/control/system/infrastructure/security/JwtTokenProvider.java
@@ -7,7 +7,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
-import java.nio.file.Files;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
@@ -29,9 +31,9 @@ public class JwtTokenProvider {
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
-                .setSigningKey(publicKey) // Valida usando a Chave Pública RSA
-                .build()
-                .parseClaimsJws(token);
+                    .setSigningKey(publicKey) // Valida usando a Chave Pública RSA
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             return false;
@@ -49,16 +51,16 @@ public class JwtTokenProvider {
     }
 
     private PublicKey loadPublicKey(Resource resource) throws Exception {
-        String key = new String(Files.readAllBytes(resource.getFile().toPath()))
-                .replace("-----BEGIN PUBLIC KEY-----", "")
-                .replace("-----END PUBLIC KEY-----", "")
-                .replaceAll("\\s+", "");
+        try (InputStream is = resource.getInputStream()) {
+            String key = new String(is.readAllBytes(), StandardCharsets.UTF_8)
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replaceAll("\\s+", "");
 
-        byte[] decode = Base64.getDecoder().decode(key);
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decode);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
-        return keyFactory.generatePublic(keySpec);
+            byte[] decode = Base64.getDecoder().decode(key);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decode);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePublic(keySpec);
+        }
     }
 }
-
