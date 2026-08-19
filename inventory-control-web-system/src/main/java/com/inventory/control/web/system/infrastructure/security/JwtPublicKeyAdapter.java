96d9ae77c8914a0049c24e7769727e084e5e3174
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-08-19 11:08:52 -0300

    Implementada autenticação com OAuth2 e token JWT no BFF

---
+++ inventory-control-web-system/src/main/java/com/inventory/control/web/system/infrastructure/security/JwtPublicKeyAdapter.java
@@ -0,0 +1,52 @@
+package com.inventory.control.web.system.infrastructure.security;
+
+import com.inventory.control.web.system.ports.out.JwtPublicKeyPort;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class JwtPublicKeyAdapter implements JwtPublicKeyPort {
+    
+    private static final Logger log = LoggerFactory.getLogger(JwtPublicKeyAdapter.class);
+
+    @Value("${jwt.public-key-path}")
+    private Resource publicKeyResource;
+
+    private RSAPublicKey publicKey;
+
+    @PostConstruct
+    public void init() throws Exception {
+        this.publicKey = loadPublicKey(publicKeyResource);
+    }
+
+    @Override
+    public RSAPublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    private RSAPublicKey loadPublicKey(Resource resource) throws Exception {
+        try (InputStream is = resource.getInputStream()) {
+            String key = new String(is.readAllBytes(), StandardCharsets.UTF_8)
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replaceAll("\\s+", "");
+
+            byte[] decode = Base64.getDecoder().decode(key);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decode);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        }
+    }
+}
